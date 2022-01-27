5348156c7ba655c76c79ed23979c999b0ff9da19
Eduardo Guanco <50039862+edufabian1@users.noreply.github.com>
2022-01-27 17:17:52 -0300

    clear files and add buttons for change months in 2022

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Button from "./components/button/Button";
 import Item from "./components/list/item/Item";
 import Title from "./components/title/Title";
+import data from "./months.json";
 
 interface IMonth {
+  id: number;
   name: string;
   countDays: number;
   firstDay: number;
 }
 
 const App = () => {
-  const currentDate: number = new Date().getDate();
-
+  const months: IMonth[] = data;
   const days = ["Lun", "Mar", "Mie", "Jue", "Vie", "Sab", "Dom"];
 
-  const month: IMonth = {
-    name: "Enero",
-    countDays: 31,
-    firstDay: 6,
-  };
+  const currentDate: number = new Date().getDate();
+  const currentMonth: number = new Date().getMonth() + 1;
 
-  const isFirstDay = (index: number): React.CSSProperties => {
-    return index === 0 ? { gridColumnStart: month.firstDay } : {};
-  };
+  const [month, setMonth] = useState<IMonth | undefined>();
+  const [isDisableBtn, setIsDisableBtn] = useState<boolean>(false);
+
+  const isFirstDay = (index: number): React.CSSProperties =>
+    index === 0 ? { gridColumnStart: month?.firstDay } : {};
+
+  useEffect(() => {
+    const test = () =>
+      setMonth(months.find((x) => x.id === currentMonth));
+
+    test();
+  }, [currentDate, currentMonth, months]);
 
   return (
     <>
       <div className="calendar">
-        <Title>Enero 2022</Title>
+        <Title>{month?.name + " 2022"}</Title>
         <ol>
           {days.map((day: string, index: number) => (
             <Item key={index} className="day-name">
               {day}
             </Item>
           ))}
-          {Array.from(Array(month.countDays)).map(
+          {Array.from(Array(month?.countDays)).map(
             (item: string, index: number) => (
               <Item
                 key={index}
-                className={currentDate === index + 1 ? "current-day" : ""}
+                className={(currentDate === index + 1 && currentMonth === month?.id) ? "current-day" : ""}
                 styles={isFirstDay(index)}
               >
                 {index + 1}
@@ -49,8 +56,26 @@ const App = () => {
         </ol>
       </div>
       <div className="container-button">
-        <Button className="button-primary previous">Anterior</Button>
-        <Button className="button-primary next">Siguiente</Button>
+        <Button
+          className="button-primary previous"
+          onClick={() => {
+            if (month && months[month.id - 2]) {
+              setMonth(months[month.id - 2]);
+            }
+          }}
+          disabled={isDisableBtn}
+        >
+          Anterior
+        </Button>
+        <Button
+          className="button-primary next"
+          onClick={() => {
+            if (month && months[month.id]) setMonth(months[month.id]);
+          }}
+          disabled={isDisableBtn}
+        >
+          Siguiente
+        </Button>
       </div>
     </>
   );
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { MouseEventHandler } from "react";
+import "./Button.css";
 
 interface IProps {
   className: string;
   children: JSX.Element | string;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+  disabled: boolean;
 }
 
 const Button = (props: IProps) => {
   return (
-    <button type="button" className={props.className}>
+    <button
+      type="button"
+      className={props.className}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
       {props.children}
     </button>
   );
